9147ac7203269b6382e932a74046ca006116d70d
leigh.morris <leigh.morris@jisc.ac.uk>
2024-09-05 15:13:09 +0100

    Lesson 5: Page template path, rendering content

--- my_wagtail_project/home/models.py
+++ my_wagtail_project/home/models.py
@@ -7,6 +7,9 @@ from wagtail.models import Page
 
 class HomePage(Page):
     # pass
+
+    template = "home/home_page.html"
+
     subtitle = models.CharField(max_length=100, blank=True, null=True)
 
     body = RichTextField(blank=True)
