c1f05ee8745815041c068943c7d2e82263b95c1d
leigh.morris <leigh.morris@jisc.ac.uk>
2024-09-05 14:55:10 +0100

    Lesson 4: Rich text, feilds panels, read only

--- my_wagtail_project/home/models.py
+++ my_wagtail_project/home/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 
-from wagtail.models import Page
 from wagtail.admin.panels import FieldPanel
+from wagtail.fields import RichTextField
+from wagtail.models import Page
 
 
 class HomePage(Page):
     # pass
     subtitle = models.CharField(max_length=100, blank=True, null=True)
 
+    body = RichTextField(blank=True)
+
     content_panels = Page.content_panels + [
-        FieldPanel("subtitle"),
+        FieldPanel("subtitle", read_only=True),
+        FieldPanel("body"),
     ]
