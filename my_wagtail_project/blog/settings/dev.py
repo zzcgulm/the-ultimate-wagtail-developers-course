0c383c809eee888f5af16f5e054ab00f02e05ce6
leigh.morris <leigh.morris@jisc.ac.uk>
2024-09-05 13:42:53 +0100

    Up to and including lesson 3

---
+++ my_wagtail_project/blog/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-7##j$jr-sm1o9s(i9_6n_0)z%6q2u)62)z%0gd*1+*rb8)q=r@"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
---
+++ my_wagtail_project/home/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+from wagtail.models import Page
+from wagtail.admin.panels import FieldPanel
+
+
+class HomePage(Page):
+    # pass
+    subtitle = models.CharField(max_length=100, blank=True, null=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("subtitle"),
+    ]
