431c0be35a1d2dd4c37a7e7b1e44455efd16bec9
leigh.morris <leigh.morris@jisc.ac.uk>
2024-09-09 13:26:16 +0100

    Lesson 6 Dynamic image serve view

--- my_wagtail_project/home/models.py
+++ my_wagtail_project/home/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
+from wagtail.models import Page
 from wagtail.admin.panels import FieldPanel
 from wagtail.fields import RichTextField
-from wagtail.models import Page
+from wagtail.images import get_image_model
 
 
 class HomePage(Page):
@@ -14,7 +15,16 @@ class HomePage(Page):
 
     body = RichTextField(blank=True)
 
+    image = models.ForeignKey(
+        get_image_model(),  # 'wagtailimages.Image' can be used as a string
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+
     content_panels = Page.content_panels + [
         FieldPanel("subtitle", read_only=True),
         FieldPanel("body"),
+        FieldPanel("image"),
     ]
