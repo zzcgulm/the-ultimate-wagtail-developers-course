7a9ae1ac188d9097ebf37694fe491e43eb57d8ca
zzcgulm <leigh.morris@jisc.ac.uk>
2024-09-27 13:44:19 +0100

    Lesson 12: Mastering Wagtail CMS Page Context

--- my_wagtail_project/blogpages/models.py
+++ my_wagtail_project/blogpages/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
+from wagtail.images import get_image_model
 
 
 class BlogIndex(Page):
@@ -22,6 +23,11 @@ class BlogIndex(Page):
         FieldPanel("body"),
     ]
 
+    def get_context(self, request):
+        context = super().get_context(request)
+        context["blogpages"] = BlogDetail.objects.live().public()
+        return context
+
 
 class BlogDetail(Page):
     # A detail page for a single blog post
@@ -29,6 +35,15 @@ class BlogDetail(Page):
     template = "blogpages/blog_detail_page.html"
 
     parent_page_types = ["blogpages.BlogIndex"]
+    subpage_types = []
+
+    image = models.ForeignKey(
+        get_image_model(),  # 'wagtailimages.Image' can be used as a string
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
 
     subtitle = models.CharField(max_length=100, blank=True)
     body = RichTextField(blank=True)
@@ -36,4 +51,5 @@ class BlogDetail(Page):
     content_panels = Page.content_panels + [
         FieldPanel("subtitle"),
         FieldPanel("body"),
+        FieldPanel("image"),
     ]
