f2a4b3c39927e5413b527ecf647e8c3164b0b0c3
zzcgulm <leigh.morris@jisc.ac.uk>
2024-09-27 15:12:29 +0100

    Lesson 14. Linking to Wagtail Pages with Validation and Custom Properties

--- my_wagtail_project/home/models.py
+++ my_wagtail_project/home/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 from wagtail.models import Page
 from wagtail.admin.panels import FieldPanel
@@ -25,8 +26,42 @@ class HomePage(Page):
         related_name="+",
     )
 
+    cta_url = models.ForeignKey(
+        "blogpages.BlogIndex",
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="+",  # Don't really need this
+    )
+
+    cta_external_url = models.URLField(blank=True, null=True)
+
     content_panels = Page.content_panels + [
         FieldPanel("subtitle", read_only=True),
+        FieldPanel("cta_url"),
+        FieldPanel("cta_external_url"),
         FieldPanel("body"),
         FieldPanel("image"),
     ]
+
+    @property
+    def get_cta_url(self):
+        if self.cta_url:
+            return self.cta_url.url
+        elif self.cta_external_url:
+            return self.cta_external_url
+        else:
+            return None
+
+    def clean(self):
+        super().clean()
+
+        errors = {}
+
+        if self.cta_url and self.cta_external_url:
+            raise ValidationError(
+                {
+                    "cta_url": "Can't have both an external and internal CTA link.",
+                    "cta_external_url": "Can't have both an external and internal CTA link.",
+                }
+            )
