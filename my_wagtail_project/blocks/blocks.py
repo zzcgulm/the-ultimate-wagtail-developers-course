06993b86d5004b57dae3ffbab7f5e95fd70969ad
Leigh-Morris_jisc <leigh.morris@jisc.ac.uk>
2025-09-01 17:13:43 +0100

    Lesson 24. Enhancing Wagtail StreamBlocks with Custom Context and Templates

--- my_wagtail_project/blocks/blocks.py
+++ my_wagtail_project/blocks/blocks.py
@@ -83,12 +83,28 @@ class CallToAction1Block(blocks.StructBlock):
     page = blocks.PageChooserBlock()
     button_text = blocks.CharBlock(max_length=100, required=False)
 
+    def get_context(self, value, parent_context=None):
+        context = super().get_context(value, parent_context=parent_context)
+        page = value.get("page")
+        button_text = value.get("button_text")
+        context["button_copy"] = button_text if button_text else f"Read: {page.title}"
+        return context
+
     class Meta:
         label = "CTA #1"
         template = "blocks/call_to_action_1_block.html"
 
 
 class ImageBlock(ImageChooserBlock):
+
+    def get_context(self, value, parent_context=None):
+        from blogpages.models import (
+            BlogDetail,
+        )  # Import here rather than at top to avoid circular ImportError
+
+        context = super().get_context(value, parent_context)
+        context["blog_posts"] = BlogDetail.objects.live().public()
+        return context
 
     class Meta:
         group = "Standalone Blocks"
